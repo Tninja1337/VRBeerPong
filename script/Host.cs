8114d3ca51815b4d6e5af6491508474d1ef648c5
Triston Guest <tristonguest@u.boisestate.edu>
2020-04-19 13:06:05 -0600

    Add files via upload

---
+++ script/Host.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Host : MonoBehaviour {
+    public OVRGrabbable grabbable;
+    public CustomNetworkManager networkManager;
+    private bool host;
+    // Use this for initialization
+    void Start () {
+        host = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (grabbable.isGrabbed == true)
+        {
+            if (host == true)
+            {
+                networkManager.StartHost();
+                host = false;
+            }
+        }
+
+    }
+}
